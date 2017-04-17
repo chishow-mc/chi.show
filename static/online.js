421ffb53b5228951eab00d5c3b9f9112ea815b55
eai04191 <eai@mizle.net>
2017-04-17 22:13:43 +0900

    サーバーオンラインチェック追加

---
+++ static/online.js
@@ -0,0 +1,16 @@
+$.ajax({
+    type: 'GET',
+    url: 'https://api.chi.show/v1/status/',
+    dataType: 'json',
+    success: function(json) {
+        if(json["servr_online"] == false) {
+            $("section.hero").animate({opacity: 0}, function() {
+                    $(this)
+                        .css('background','linear-gradient(141deg,#9E0032 0,#D1001F 71%,#EB0400 100%)')
+                        .animate({opacity: 1});
+                    $('.hero.is-primary .tabs.is-boxed li.is-active a')
+                        .css('color','#D1001F');
+            });
+        }
+    }
+});
